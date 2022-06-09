f055ab21be92e73bdb737a69a40f18516cf221bb
NoahBruno <94698680+NoahBruno@users.noreply.github.com>
2022-06-09 14:43:17 -0500

    Xo's

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+//Initiation
+//-------------------------------------------------------------
+
 function multiply(a, b){
   return a * b
 }
 
 console.log(multiply(5,5))
 
-let array = [];
+//Multiples of 3 and 5
+//----------------------------------------------------------------------------------------------
 
 function solution(number){
   let sum= 0;
@@ -22,4 +26,38 @@ function solution(number){
   return sum;
 }
 console.log(solution(30))
+
+//Array.diff
+//-------------------------------------------------------------------------------------
+
+function arrayDiff(a, b) {
+  let diff = a.filter(x => !b.includes(x));
+  return diff;
+}
+console.log(arrayDiff([1,2,2,2,2,3], [2,1]))
+
+//Find the odd int
+//-----------------------------------------------------------------------------------------
+function findOdd(A) {
+  //happy coding!
+  return 0;
+}
+
+//Exes and Ohs
+//----------------------------------------------------------------------------------------
+
+function XO(str) {
+  let xNumber = '';
+  let oNumber = '';
+  for (let i=0; i<str.length; i++) {
+    if (str[i].toLowerCase() === 'x') {
+      xNumber += str[i];
+    } else if (str[i].toLowerCase() === 'o') {
+      oNumber += str[i];
+    }
+  }
+  return xNumber.length === oNumber.length ;
+}
+
+console.log(XO("xooxxox"))
 
