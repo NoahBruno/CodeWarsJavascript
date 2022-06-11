5a866039553d1797ea3701ac36312f1f74e098df
NoahBruno <94698680+NoahBruno@users.noreply.github.com>
2022-06-10 22:58:26 -0500

    accum

--- js/main.js
+++ js/main.js
@@ -61,3 +61,26 @@ function XO(str) {
 
 console.log(XO("xooxxox"))
 
+//Mumbling
+//----------------------------------------------------------------------------------------
+
+
+function accum(s){
+  let array = []
+  for (let i =0; i < s.length; i++){
+    array.push(format(s[i], i +1))
+  }
+  return array.join('-');
+}
+
+function format(s, num){
+  let letter = s.toUpperCase()
+
+  while(letter.length !== num){
+    letter += s.toLowerCase()
+  }
+  return letter
+}
+
+console.log(accum("abcd"))
+
